037bed7bb07392f662dd7445dcbcaf77f7f11cc5
Adam Saxén <adam.saxen@gmail.com>
2017-01-13 21:18:09 +0100

    fix to install_server

--- tools/setup/install_server.sh
+++ tools/setup/install_server.sh
@@ -11,6 +11,7 @@ pip install 'protobuf>=3.1.0'
 pip install 'paho-mqtt>=1.2'
 pip install "MySQL-python>=1.2.5"
 pip install 'mock>=2.0.0'
+pip install django
 
 # install nabton SDK pynab library
 pip install ioant
@@ -20,3 +21,7 @@ pip install ioant_mysqlhelper
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
+
+sudo apt-get install mosquitto
+
+sudo npm install -g pm2
