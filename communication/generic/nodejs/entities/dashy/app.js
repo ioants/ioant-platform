4cc7d834ab08503a76860146ac12d4d47d4145c0
Simon Lövgren <lovgren.simon@gmail.com>
2017-01-25 12:00:54 +0100

    added MQTT.js

--- communication/generic/nodejs/entities/dashy/app.js
+++ communication/generic/nodejs/entities/dashy/app.js
@@ -8,27 +8,54 @@
 //////////// MODULES ///////////
 var Express = require('express');
 var Promise = require('bluebird');
-var Logger = require('./utils/logger.js');
-var Loader = require('./utils/loader.js');
+var Logger  = require('./utils/logger.js');
+var Loader  = require('./utils/loader.js');
+var MQTT    = require('mqtt');
 
 //////////// VARIABLES ///////////
 // -- Express web server
-var app = Express();
+var express = Express();
 var server = null;
+var server_running = false;
 var config = null;
 
+// -- MQTT
+var mqtt = null;
+
 // Initial setup
-app.use(require('./routes.js'));
+express.use(require('./routes.js'));
 
 //////////// FUNCTIONS ///////////
 function startApplication() {
-    // start application
-    server = app.listen(config.dashy.port, function () {
-        Logger.log('info', "Dashy started, listening on " +
-                   server.address().address + ":" +
+    // start web server
+    server = express.listen(config.dashy.port, function () {
+        Logger.log('info', 'Dashy webserver started, listening on ' +
+                   (server.address().address ? server.address().address : 'localhost') + ':' +
                    server.address().port
-                  ); 
-   });
+                  );
+        server_running = true;
+    });
+
+    // start MQTT client
+    {
+        var broker = 'mqtt://' + config.mqtt.broker + (config.mqtt.port ? ':' + config.mqtt.port : '');
+        var options = {};
+        Logger.log('info', 'Connecting to MQTT (' + broker  +').');
+        if(config.mqtt.user) {
+            options.username = config.mqtt.user.trim();
+            Logger.log('info', 'As user: "' + options.username + '"');
+        } else {
+            Logger.log('info', 'As user: NO.');
+        }
+        if(config.mqtt.password) {
+            options.password = config.mqtt.password.trim();
+            Logger.log('info', 'Using password.')
+        } else {
+            Logger.log('info', 'Using password: NO.')
+        }
+        mqtt = MQTT.connect(broker);
+
+    }
 }
 
 function main() {
@@ -38,8 +65,21 @@ function main() {
         Logger.log('info', 'Loaded configuration.', {configuration: config});
         startApplication();
     }).catch(function(e){
-        Logger.log('error', 'Failed to start application (Dashy)');
+        Logger.log('error', 'Failed to start application (Dashy)', e);
+        terminate();
     });
+}
+
+function terminate() {
+    Logger.log('info', 'Terminating the application, good bye!');
+    // Close web server
+    if(server !== null && server_running) {
+        server.close();
+    }
+    // Close MQTT client
+    if(mqtt !== null && mqtt.connected) {
+        mqtt.end();
+    }
 }
 
 //////////// ENTRY POINT  ///////////
@@ -48,9 +88,8 @@ if (require.main === module) {
 }
 
 process.on('SIGINT', function(){
-    logger.log('info', 'Gracefully shutting down from SIGINT (CTRL-C)');
-    if(server !== null) {
-        server.close();
-    }
+    console.log(""); // Move to line after printed ^C
+    Logger.log('info', 'Gracefully shutting down from SIGINT (CTRL-C)');
+    terminate();
     process.exit();
 });
