01f50b88a61a33d19c52e35a98733b2a261862b5
benny.saxen@gmail.com <benny.saxen@gmail.com>
2017-02-06 17:04:14 +0100

    improved stepper control

--- communication/esp8266/arduino/devices/boilerplates/B-104-stepper/src/main.cpp
+++ communication/esp8266/arduino/devices/boilerplates/B-104-stepper/src/main.cpp
@@ -102,27 +102,32 @@ void on_message(Core::Topic received_topic, ProtoIO* message){
 
     if (received_topic.message_type == ProtoIO::MessageTypes::RUNSTEPPERMOTORRAW)
     {
+        ULOG_DEBUG << "Stepper awake";
         RunStepperMotorRawMessage *msg = static_cast<RunStepperMotorRawMessage*>(message);
         if(msg->data.number_of_step < 1 || msg->data.number_of_step > 500) return;
         if(msg->data.delay_between_steps < 1 || msg->data.delay_between_steps > 1000) return;
 
         if(msg->data.step_size == RunStepperMotorRaw_StepSize_FULL_STEP)
         {
+            ULOG_DEBUG << "Stepper FULL STEP";
             digitalWrite(MS1,LOW);
             digitalWrite(MS2,LOW);
         }
         else if (msg->data.step_size == RunStepperMotorRaw_StepSize_HALF_STEP)
         {
+            ULOG_DEBUG << "Stepper HALF STEP";
             digitalWrite(MS1,HIGH);
             digitalWrite(MS2,LOW);
         }
         else if (msg->data.step_size == RunStepperMotorRaw_StepSize_QUARTER_STEP)
         {
+            ULOG_DEBUG << "Stepper QUARTER STEP";
             digitalWrite(MS1,LOW);
             digitalWrite(MS2,HIGH);
         }
         else // default fullstep
         {
+            ULOG_DEBUG << "Stepper DEFAULT FULL STEP";
             digitalWrite(MS1,LOW);
             digitalWrite(MS2,LOW);
         }
@@ -142,5 +147,6 @@ void on_message(Core::Topic received_topic, ProtoIO* message){
 
         digitalWrite(MS1,LOW);
         digitalWrite(MS2,LOW);
+        ULOG_DEBUG << "Stepper sleeping";
     }
 }
