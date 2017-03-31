1a074c50b860ff342628813c0a1619b4e4db5eb3
saxen <saxen@localhost.localdomain>
2017-03-31 16:48:39 +0200

    removed unused db-creator script

--- storage/collector-python/main.py
+++ storage/collector-python/main.py
@@ -5,7 +5,7 @@ import os
 import logging
 
 logging.basicConfig(filename='logs/output.log',
-                    level=logging.DEBUG,
+                    level=logging.INFO,
                     format='%(asctime)s %(name)-5s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M')
 console = logging.StreamHandler()
